e8f6911911287911be8729b999c7dac251192b95
bradybellini <bellinibrady65@gmail.com>
2019-10-08 21:17:06 -0700

    clean up

--- cogs/config.py
+++ cogs/config.py
@@ -4,13 +4,7 @@ from discord.ext import commands
 
 
 class Config(commands.Cog):
-
-    def __init__(self):
-        with open('keys.json', 'r') as rk, open('config.json', 'r') as rc:
-            keys, config = json.load(rk), json.load(rc)
-        self.apikey = keys['discordapi']
-        self.test = config['test']
-
+    pass
 
 def setup(client):
     client.add_cog(Config())
