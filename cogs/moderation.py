c7700cce2d1d0bd843574d36b033c0677ded98f4
bradybellini <bellinibrady65@gmail.com>
2019-10-10 19:12:37 -0700

    purge errors, ban and ban errors

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,16 +8,20 @@ class Moderation(commands.Cog, name='Moderation Commands'):
 
     @commands.has_permissions(manage_messages=True)
     @commands.command()
-    async def purge(self, ctx, *, amount:int=None):
-        try:
-            if not amount:
-                # add default error embed here
-                await ctx.send('User must specify the number of messages for Marvin to delete')
-            else:
-                deleted = await ctx.message.channel.purge(limit=amount)
-                await ctx.send(f'Messages deleted by {ctx.message.author.mention}: `{len(deleted)}`')
-        except:
-            await ctx.send('Marvin can not purge messages here.')
+    async def purge(self, ctx, amount:int):
+        deleted = await ctx.message.channel.purge(limit=amount + 1)
+        await ctx.send(f'Messages deleted by {ctx.message.author.mention}: `{len(deleted)}`')
+
+    @purge.error
+    async def purge_error(self, ctx, error):
+        embed = discord.Embed(title="Try: m.purge [message amount]", colour=0xd95454)
+        embed.set_author(name=f"{error}", url="https://discordapp.com")
+        embed_badarg = discord.Embed(title="Try: m.purge [message amount]", colour=0xd95454)
+        embed_badarg.set_author(name="Message amount must be a number.", url="https://discordapp.com")
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send(embed=embed_badarg)
 
     @commands.has_permissions(kick_members=True)
     @commands.command()
@@ -33,6 +37,23 @@ class Moderation(commands.Cog, name='Moderation Commands'):
     async def kick_error(self, ctx, error):
         # add error for trying to kick someone of equal or greater permissions and thats it for perms
         embed = discord.Embed(title="Try: m.kick [user] <reason>", colour=0xd95454)
+        embed.set_author(name=f"{error}", url="https://discordapp.com")
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(embed=embed)
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command()
+    async def ban(self, ctx, user:discord.Member, *, reason=None):
+        await ctx.guild.ban(user=user, reason=reason)
+        await ctx.send(f'{user} has been banned')
+
+
+    @ban.error
+    async def ban_error(self, ctx, error):
+        # add error for trying to banning someone of equal or greater permissions and thats it for perms
+        embed = discord.Embed(title="Try: m.ban [user] <reason>", colour=0xd95454)
         embed.set_author(name=f"{error}", url="https://discordapp.com")
         if isinstance(error, commands.BadArgument):
             await ctx.send(embed=embed)
