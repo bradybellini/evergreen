084e732061757349dcd3a8c54e44c24f4f1bc407
bradybellini <bellinibrady65@gmail.com>
2019-10-11 22:25:44 -0700

    details

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
 
-class Moderation(commands.Cog, name='Moderation Commands'):
-
+class Moderation(commands.Cog, name='moderation'):
+    """Moderation related commands"""
     def __init__(self, client):
         self.client = client
 
     @commands.has_permissions(manage_messages=True)
     @commands.command()
     async def purge(self, ctx, amount:int):
+        """: deletes specified amount of messages"""
         deleted = await ctx.message.channel.purge(limit=amount + 1)
         await ctx.send(f'Messages deleted by {ctx.message.author.mention}: `{len(deleted)}`')
 
@@ -26,6 +27,7 @@ class Moderation(commands.Cog, name='Moderation Commands'):
     @commands.has_permissions(kick_members=True)
     @commands.command()
     async def kick(self, ctx, user:discord.Member, *, reason=None):
+        """: Kick a user"""
         if not reason:
             await ctx.guild.kick(user=user, reason='No reason provided')
             # insert embed with user kicked and reason for kicking
@@ -35,7 +37,6 @@ class Moderation(commands.Cog, name='Moderation Commands'):
 
     @kick.error
     async def kick_error(self, ctx, error):
-        # add error for trying to kick someone of equal or greater permissions and thats it for perms
         embed = discord.Embed(title="Try: m.kick [user] <reason>", colour=0xd95454)
         embed.set_author(name=f"{error}", url="https://discordapp.com")
         embed_forb = discord.Embed(title="Try: m.kick [user] <reason>", colour=0xd95454)
@@ -51,6 +52,7 @@ class Moderation(commands.Cog, name='Moderation Commands'):
     @commands.has_permissions(ban_members=True)
     @commands.command()
     async def ban(self, ctx, member:discord.Member, *, reason=None):
+        """: Ban a user"""
         await ctx.guild.ban(user=member, reason=reason)
         await ctx.send(f'{member} has been banned')
 
@@ -71,6 +73,7 @@ class Moderation(commands.Cog, name='Moderation Commands'):
     @commands.has_permissions(ban_members=True)
     @commands.command()
     async def unban(self, ctx, member:discord.Member, *, reason=None):
+        """: Unban a user"""
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split('#')
         for banned_entry in banned_users:
