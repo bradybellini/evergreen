d8365b36f3ecd3f3f18524f343a89a96861b8f79
Brady <bellinibrady65@gmail.com>
2019-10-17 12:58:01 -0700

    asyncpg testing

--- bot.py
+++ bot.py
@@ -27,10 +27,11 @@ with open('config.json', 'r') as rc:
 
 client = commands.Bot(command_prefix=config['prefix'], owner_id=config['ownerid'], description="Hello, I am Marvin. I look forward to your company. \n Please visit hellomarvin.org for any furthur help")
 
-
+#mess around with pooling size and when innactive connections should be terminiated.
+#Look at the asyncpg docs with connection pooling. This needs to be optimized because I think it will have alot to do with the repsonse speed.
 async def create_db_pool():
     credentials = 'postgresql://marvinclient:' + pgpswd + '@pgs-marvin-sfo2-1-do-user-4855641-0.db.ondigitalocean.com:25060/marvin?sslmode=require'
-    client.pg_conn = await asyncpg.create_pool(credentials)
+    client.pg_conn = await asyncpg.create_pool(dsn=credentials, min_size=2, max_size=22)
 
 
 @client.event
