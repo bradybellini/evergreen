0d776057157b211e390dfa7d3655479058a3f5dd
bradybellini <bellinibrady65@gmail.com>
2019-10-23 21:12:25 -0700

    testing aiosqlite for queries

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,6 +1,7 @@
 import discord
 import asyncpg
 import asyncio
+import aiosqlite
 from apikeys import pgpswd
 from datetime import datetime
 from discord.ext import commands
@@ -10,18 +11,33 @@ class Tickets(commands.Cog, name="tickets"):
     def __init__(self, client):
         self.client = client
         self.loop = asyncio.get_event_loop()
-        # self.credentials = 'postgresql://marvinclient:' + pgpswd + '@pgs-marvin-sfo2-1-do-user-4855641-0.db.ondigitalocean.com:25060/marvin?sslmode=require'
+        self.credentials = 'postgresql://doadmin:' + pgpswd + '@vergreen-psgsql-sf2-do-user-4855641-0.db.ondigitalocean.com:25060/defaultdb?sslmode=require'
+
 # DO private networking is not working from droplet to db
     @commands.group(invoke_without_command=True)
     async def ticket(self, ctx, content=None):
-        await self.client.pg_conn.execute('''INSERT INTO guilds()''')
+        await self.client.pg_conn.execute('''INSERT INTO guilds''')
         guild_id = await self.client.pg_conn.fetch('''SELECT * FROM guilds''')
+        connect = await asyncpg.connect(self.credentials)
+        values = await connect.fetch('''SELECT * FROM guilds''')
+        await ctx.send(guild_id[0]['guild_id'])
+        await connect.close()
+        await ctx.send(values)
+        db = await aiosqlite.connect('evergreen.db')
+        cursor = await db.execute(f'SELECT tickets FROM disabled_modules WHERE guild_id = {ctx.guild.id}')
+        result = await cursor.fetchone()
+        disabled = 'The server admin has disabled this command or module'
+        if result[0]:
+            await ctx.send(disabled)
+        else:
+            await ctx.send('set up ticket help and setup')
+            
+        # await self.client.pg_conn.execute('''INSERT INTO guilds''')
+        # guild_id = await self.client.pg_conn.fetch('''SELECT * FROM guilds''')
         # connect = await asyncpg.connect(self.credentials)
         # values = await connect.fetch('''SELECT * FROM guilds''')
-        await ctx.send(guild_id[0]['guild_id'])
+        # await ctx.send(guild_id[0]['guild_id'])
         # await connect.close()
-
-
 
     @commands.has_permissions(administrator=True)
     @ticket.command()
@@ -35,11 +51,13 @@ class Tickets(commands.Cog, name="tickets"):
     async def new(self, ctx, *, content):
         embed = discord.Embed(title="New Ticket - Ticket ID", colour=0x90ff)
         embed.add_field(name="Ticket author", value=f"{ctx.message.author}")
-        embed.add_field(name="Content", value=f"{content}", inline=True)
-        embed.add_field(name="Status", value="Open", inline=True)
-        embed.add_field(name="Created", value=f"{datetime.now()}", inline=True)
+        embed.add_field(name="Content", value=f"{content}", inline=False)
+        embed.add_field(name="Status", value="Open", inline=False)
+        embed.add_field(name="Created", value=f"{datetime.now()}", inline=False)
         channel = self.client.get_channel(632070457302188034)
         await channel.send(embed=embed)
+
+
 
     @new.error
     async def new_ticket_error(self, ctx, error):
