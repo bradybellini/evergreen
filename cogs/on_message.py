a892eb4a7577702e41e7f7a2348ccbf9283d6b0b
bradybellini <bellinibrady65@gmail.com>
2020-01-24 02:40:11 -0700

    changed to ticket for testing

--- cogs/on_message.py
+++ cogs/on_message.py
@@ -18,9 +18,9 @@ class OnMessage(commands.Cog, name='On Message Listeners and Events'):
         if message.author == self.client.user.id: #or message.author.guild_permissions.manage_messages:
             return
         
-        if message.content.startswith('m.report') and message.channel.type == discord.ChannelType.text:
+        if message.content.startswith('m.ticket') and message.channel.type == discord.ChannelType.text:
             await message.delete()
-            await message.channel.send(f'{message.author.mention} Reports must be made in a DM with Marvin')
+            await message.channel.send(f'{message.author.mention} Your ticket has been submitted, if you did not receive a DM from Marvin, let a staff member know.')
 
 
 def setup(client):
