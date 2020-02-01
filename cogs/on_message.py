966384928f2b3e0c1d751a1cd917fc25730025ed
bradybellini <bellinibrady65@gmail.com>
2020-01-31 21:39:50 -0700

    small

--- cogs/on_message.py
+++ cogs/on_message.py
@@ -15,13 +15,13 @@ class OnMessage(commands.Cog, name='On Message Listeners and Events'):
 
     @commands.Cog.listener()
     async def on_message(self, message:discord.Message):
-        if discord.ChannelType.private:
-            return
+        # if discord.ChannelType.private:
+        #     return
             
         if message.author == self.client.user.id or message.author.guild_permissions.manage_messages:
             return
         
-        if message.content.startswith('m.ticket') and message.channel.type == discord.ChannelType.text:
+        if message.content.startswith('m.ticket ') and message.channel.type == discord.ChannelType.text:
             await message.delete()
             await message.channel.send(f'{message.author.mention} Your ticket has been submitted, if you did not receive a DM from Marvin, let a staff member know.')
 
