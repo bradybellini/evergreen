1c455f28b85b0ab80eafee9eaca8030139e956f3
bradybellini <bbellini@pm.me>
2020-02-10 20:41:25 -0700

    Update errors.py

--- cogs/errors.py
+++ cogs/errors.py
@@ -7,7 +7,7 @@ class Error(commands.Cog, name='Errors'):
         self.client = client
 
     async def on_command_error(self, ctx, error):
-        pass
+        await ctx.send(error)
 
 def setup(client):
     client.add_cog(Error(client))
