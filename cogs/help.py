091136ac8fc436b2ee463b5e3731eab7517490b7
bradybellini <bbellini@pm.me>
2020-03-23 20:15:45 -0700

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -12,11 +12,11 @@ class Help(commands.Cog, name="help"):
         embed = discord.Embed(colour=0x74ff90, title="InfinityCraft 2.∞ Discord Help",
                               description="[] = required argument\n<> = optional argument\nThe `|` means you can use either word for the command.\nFor example to submit a ticket you can use `m.ticket` **or** `m.report`,\nwhich is shown below using `|`.")
         embed.add_field(name="**Ticket Help**",
-                        value="`m.ticket|report new [content]` - Create a new ticket or report.\n**Please submit tickets and reports in a direct message to Marvin.**")
+                        value="`m.ticket|report new [content]` - Create a new ticket or report.\n**Please submit tickets and reports in a direct message to Marvin.**", inline=False)
         embed.add_field(name="**Server Info Help**",
-                        value="`m.server status` - Displays whether the server is online with player count.\n`m.server ip` - Displays the server IP.")
+                        value="`m.server status` - Displays whether the server is online with player count.\n`m.server ip` - Displays the server IP.\n`m.rules` - List the Discord and Minecraft Server rules.", inline=False)
         embed.add_field(name="Other Help",
-                        value="`m.mojang` - Check the status of Mojang & Minecraft services.")
+                        value="`m.mojang` - Check the status of Mojang & Minecraft services.", inline=False)
         embed.timestamp = datetime.utcnow()
         embed.set_footer(
             text="Marvin", icon_url=f'{self.client.user.avatar_url}')
