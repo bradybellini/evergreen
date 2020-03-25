f45617b1b6611430f635f0c7d47f0d944f15ea8d
bradybellini <bbellini@pm.me>
2020-03-25 15:55:33 -0700

    Update meta.py

--- cogs/meta.py
+++ cogs/meta.py
@@ -53,11 +53,15 @@ class Meta(commands.Cog, name="meta"):
     @commands.command()
     async def welcomemessage(self, ctx):
         help_channel: TextChannel = self.client.get_channel(623702240523321356)
+        ticket_channel: TextChannel = self.client.get_channel(
+            673097926825738250)
+        rules_channel: TextChannel = self.client.get_channel(
+            622988649113583637)
         join_message = f""":small_orange_diamond: Welcome to InfinityCraft 2.∞! If you need help make sure to check out the {help_channel.mention} channel or run the command `m.help` to get Marvin(me) to give you command help within the server. 
 :small_orange_diamond: I may take some time to respond to your command so please be patient. I am a cactus after all. 
 :small_blue_diamond: Make sure to run the command `/discord link` in Minecraft to link your Minecraft and Discord account and follow the directions given. You should get the Voyager rank as well as some other perks. 
-:small_blue_diamond: To submit a report or ticket, check out the #ticket channel. 
-:small_blue_diamond: Last of all, make sure to follow all the rules posted in the #rules channel. 
+:small_blue_diamond: To submit a report or ticket, check out the {ticket_channel.mention} channel. 
+:small_blue_diamond: Last of all, make sure to follow all the rules posted in the {rules_channel.mention} channel. 
 :small_blue_diamond: If you have any further questions, feel free to ask a staff member or other members of the community.
 
 Server IP: `mc.gamersgrove.net`
